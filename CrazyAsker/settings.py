e3f512576af830884ed042e339629c689a05acf9
Gorbunov Pavel <p.gorbunov@corp.mail.ru>
2020-05-01 16:08:22 +0300

    setting media_root

--- CrazyAsker/settings.py
+++ CrazyAsker/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -135,4 +137,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "public/static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = f'{BASE_DIR}/media/'
+MEDIA_ROOT = MEDIA_DIR
--- CrazyAsker/urls.py
+++ CrazyAsker/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('asker.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'asker.views.page_not_found'
